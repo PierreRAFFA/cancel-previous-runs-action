65619c2d2e685c31600fb290e7080ee7f192415b
Pierre RAFFA <pierreraffa@192.168.1.221>
2023-07-16 21:31:35 +0100

    Update README

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Cancel Previous Runs Action 
-#### (merge_group event only at the moment)
 
-This GitHub action is responsible to cancel any previous runs for the current workflow that are:  
-- triggered by `merge_group` event  
-- related to the same PR number than the current run  
-- `in_progress` status.  
+This GitHub action is responsible to cancel any previous runs for the current workflow.
+It supports 3 events:  
+- pull_request  
+- push  
+- merge_group  
 
+### For `merge_group` event:
 The cancellation of the previous runs could be required when:
 - the merge queue has entries running concurrently  
 - the first entry fails and all the next ones will have to run again  
@@ -18,13 +19,12 @@ This result in multiple runs executed for the same PR queued.
 ## Usage 
 
 Place this job at the beginning of your workflow.  
-Once the job running, it will check for all runs related to the same PR than the current run and cancel all of them.  
+Once the job running, it will check for all runs related to the same context than the current run and cancel all of them.  
 
 ```yaml
 jobs:
   cancel-previous-runs:
     runs-on: ubuntu-latest
-    if: github.event_name == 'merge_group'
     steps:
       - name: Cancel Previous Runs
         uses: pierreraffa/cancel-previous-runs-action
--- cancel.sh
+++ cancel.sh
@@ -6,20 +6,22 @@ workflow=$GITHUB_WORKFLOW
 event=$GITHUB_EVENT_NAME
 # Specifies the current run id we don't want to touch
 current_run_id=$GITHUB_RUN_ID
+# Get current run `createdAt`
+current_run_created_at=$(gh run view $current_run_id --json createdAt --jq '.[]')
 
-###########################
-## event: merge_group
-###########################
-if [ "$event" = "merge_group" ]; then
+if [ "$event" == "merge_group" ]; then
+  ###########################
+  ## event: merge_group
+  ###########################
   # Specifies the ref branch which is the temporary branch created by Gh and formatted such as `gh-readonly-queue/main/pr-{num}-{sha}`
   ref=$GITHUB_REF
   # extract pr identifier formatted as `pr-{pr}`
   pr_identifier=$(echo $ref | grep -oP 'pr-\d+')
-  # Get current run `createdAt`
-  current_run_created_at=$(gh run view $current_run_id --json createdAt --jq '.[]')
+  
 
   # Prints
   echo "workflow:                 $workflow"
+  echo "event:                    $event"
   echo "ref:                      $ref"
   echo "current_run_id:           $current_run_id"
   echo "pr_identifier:            $pr_identifier"
@@ -32,27 +34,54 @@ if [ "$event" = "merge_group" ]; then
   #   - the run is in progress
   #   - the run is NOT the current one
   #   - the run is created before the current one
-  runs=$(gh run list -e merge_group --workflow $workflow -L 200 \
-    --json databaseId,workflowName,createdAt,status,number,headBranch  \
+  runs=$(gh run list --workflow $workflow -L 200 \
+    --json databaseId,workflowName,createdAt,event,status,number,headBranch  \
     --jq 'map(select(.headBranch | contains("'${pr_identifier}'"))
-          | select(.status == "completed")
+          | select(.event == "'${event}'")
+          | select(.status == "in_progress")
           | select(.databaseId != '"${current_run_id}"')
           | select(.createdAt < "'${current_run_created_at}'"))')
-  echo "runs for cancellation: $runs"
 
-  # Get run ids
-  run_ids=$(echo $runs | jq -c '[ .[] | .databaseId ]')
-  echo "runs ids for cancellation: $run_ids"
+elif [ "$event" == "pull_request" ] || [ "$event" == "push" ]; then
+  ###########################
+  ## event: pull_request / push
+  ###########################
 
-  # Cancels the run
-  echo $run_ids | jq '.[]' | xargs -I{} bash -c "echo 'Cancelling run {}...' && gh run cancel {}"
-else
-###########################
-## event: pull_request/push
-###########################
   # Specifies the ref branch which is the temporary branch created by Gh and formatted such as `gh-readonly-queue/main/pr-{num}-{sha}`
-  ref=$GITHUB_REF
-  echo "GITHUB_SHA: ${GITHUB_SHA}"
-  echo "GITHUB_REF: ${GITHUB_REF}"
-  echo "GITHUB_HEAD_REF: ${GITHUB_HEAD_REF}"
+  if [ "$event" == "pull_request" ]; then
+    ref=$GITHUB_HEAD_REF
+  elif [ "$event" == "push" ]; then
+    ref=$GITHUB_REF_NAME
+  fi
+
+  # Prints
+  echo "workflow:                 $workflow"
+  echo "event:                    $event"
+  echo "ref:                      $ref"
+  echo "current_run_id:           $current_run_id"
+  echo "current_run_created_at:   $current_run_created_at"
+
+  # Returns the list of runs which respect all the following conditions:
+  #   - event is pull_request / push
+  #   - worflow is the one specified
+  #   - the run is related to the same ref than the current run
+  #   - the run is in progress
+  #   - the run is NOT the current one
+  #   - the run is created before the current one
+  runs=$(gh run list --workflow $workflow -L 200 \
+    --json databaseId,workflowName,createdAt,event,status,number,headBranch \
+    --jq 'map(select(.headBranch == "'${ref}'")
+          | select(.event == "'${event}'")
+          | select(.status == "in_progress")
+          | select(.databaseId != '"${current_run_id}"')
+          | select(.createdAt < "'${current_run_created_at}'"))')
 fi
+
+echo "Runs for cancellation: $runs"
+
+# Get run ids
+run_ids=$(echo $runs | jq -c '[ .[] | .databaseId ]')
+echo "runs ids for cancellation: $run_ids"
+
+# Cancels the run
+echo $run_ids | jq '.[]' | xargs -I{} bash -c "echo 'Cancelling run {}...' && gh run cancel {} || true"
